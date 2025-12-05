2ce9cff5f11db7f69f710e2795c80933d7482210
Jaša Marolt <jasa.marolt@gmail.com>
2025-12-05 18:52:46 +0100

    lazygit

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
+vim.opt.termguicolors = true
 vim.keymap.set("n", "<leader>cne", function()
   --vim.api.nvim_exec2('!echo "-------------------------------NEWLINE---------------------------------"', {})
   vim.api.nvim_exec2("!ninja -C build && ./build/main", {})
