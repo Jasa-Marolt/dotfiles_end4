0f153d5fc681b2c4e0df73396f5a515f39e58066
Jaša Marolt <jasa.marolt@gmail.com>
2025-12-05 22:02:29 +0100

    better paste

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
 vim.opt.termguicolors = true
+
 vim.keymap.set("n", "<leader>cne", function()
   vim.api.nvim_exec2("!ninja -C build && ./build/main", {})
 end, { silent = true })
@@ -9,4 +11,13 @@ vim.keymap.set("n", "<c-/>", function()
   Snacks.terminal(nil, { cwd = LazyVim.root() })
 end, { desc = "Terminal (Root Dir)" })
 
---
+--asdasdadyy
+--asdasdadyy
+--asdasdadyy
+--asdasdadyy
+--asdasdadyy
+--asdasdadyy
+--testtest
+--testtest
+--testtest
+--testtest
