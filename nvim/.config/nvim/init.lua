17501db5a4d4c5e7e80128988816dab93be143b1
Jasa-Marolt <jasa.marolt@gmail.com>
2025-12-28 13:08:16 +0100

    better movement

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -12,5 +12,99 @@ map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window" })
 map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window" })
 map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window" })
 map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window" })
+map('i', '<C-h>', '<Left>', { desc = 'Move left' })
+map('i', '<C-j>', '<Down>', { desc = 'Move down' })
+map('i', '<C-k>', '<Up>', { desc = 'Move up' })
+map('i', '<C-l>', '<Right>', { desc = 'Move right' })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch', -- Color group (try 'Visual' or 'Substitute'
+      timeout = 100,         -- Duration in milliseconds
+    })
+  end,
+})
+
+local function save_and_format()
+    vim.lsp.buf.format()
+Snacks.notifier.notify("File Saved & Formatted", "info", { 
+        id = "save_notif",
+        title = "System", 
+	style = "compact",
+	timeout = 1000
+    })
+end
+
+-- Normal mode
+vim.keymap.set('n', '<C-s>', save_and_format)
+
+-- Visual mode
+vim.keymap.set('v', '<C-s>', function()
+    save_and_format()
+    vim.api.nvim_input('<Esc>')
+end)
+
+-- Insert mode
+vim.keymap.set('i', '<C-s>', function()
+    save_and_format()
+    -- Stay in Insert mode
+end)
 
 
+
+
+
+
+
+
+
+
+
+
+--lsp progress using snacks notif 
+---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
+local progress = vim.defaulttable()
+vim.api.nvim_create_autocmd("LspProgress", {
+  ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+    if not client or type(value) ~= "table" then
+      return
+    end
+    local p = progress[client.id]
+
+    for i = 1, #p + 1 do
+      if i == #p + 1 or p[i].token == ev.data.params.token then
+        p[i] = {
+          token = ev.data.params.token,
+          msg = ("[%3d%%] %s%s"):format(
+            value.kind == "end" and 100 or value.percentage or 100,
+            value.title or "",
+            value.message and (" **%s**"):format(value.message) or ""
+          ),
+          done = value.kind == "end",
+        }
+        break
+      end
+    end
+
+    local msg = {} ---@type string[]
+    progress[client.id] = vim.tbl_filter(function(v)
+      return table.insert(msg, v.msg) or not v.done
+    end, p)
+
+    local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+    vim.notify(table.concat(msg, "\n"), "info", {
+      id = "lsp_progress",
+      title = client.name,
+      opts = function(notif)
+        notif.icon = #progress[client.id] == 0 and " "
+          or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+      end,
+    })
+  end,
+})
