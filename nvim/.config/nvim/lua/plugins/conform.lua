9accc3dd984f819aa3101d98566b0ef23c5009ef
Jasa-Marolt <jasa.marolt@gmail.com>
2026-01-27 22:23:41 +0100

    typescript linting

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,36 +1,40 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" }, -- This triggers the plugin before saving
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      python = { "isort", "black" },
-      rust = { "rustfmt" },
-      javascript = { "prettierd", "prettier", stop_after_first = true },
-      cpp = { "clang-format" },
-      c = { "clang-format" },
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" }, -- This triggers the plugin before saving
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            python = { "isort", "black" },
+            rust = { "rustfmt" },
+            javascript = { "prettierd", "prettier", stop_after_first = true },
+            javascriptreact = { "prettier" },
+            typescriptreact = { "prettier" },
+            html = { "prettier" },
+            cpp = { "clang-format" },
+            c = { "clang-format" },
+        },
+        formatters = {
+            stylua = {
+                prepend_args = { "--indent-type", "Spaces", "--indent-width", "4" },
+            },
+            ["clang-format"] = {
+                prepend_args = { "--style={IndentWidth: 4}" },
+            },
+            prettier = {
+                prepend_args = { "--tab-width", "4" },
+            },
+            prettierd = {
+                prepend_args = { "--tab-width", "4" },
+            },
+        },
+        -- Correct way to set global defaults
+        default_format_opts = {
+            lsp_format = "fallback",
+        },
+        format_on_save = {
+            timeout_ms = 500,
+            lsp_format = "fallback",
+        },
     },
-    formatters = {
-      stylua = {
-        prepend_args = { "--indent-type", "Spaces", "--indent-width", "4" },
-      },
-      ["clang-format"] = {
-        prepend_args = { "--style={IndentWidth: 4}" },
-      },
-      prettier = {
-        prepend_args = { "--tab-width", "4" },
-      },
-      prettierd = {
-        prepend_args = { "--tab-width", "4" },
-      },
-    },
-    -- Correct way to set global defaults
-    default_format_opts = {
-      lsp_format = "fallback",
-    },
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_format = "fallback",
-    },
-  },
 }
+
