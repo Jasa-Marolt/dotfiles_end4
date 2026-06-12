9ca15399417d89338cf0a031a83137a4d945b96d
TheWanderer <jasa.marolt@gmail.com>
2026-06-12 18:24:30 +0200

    bg and colorscheme plugins

---
+++ nvim/.config/nvim/lua/plugins/bg.lua
@@ -0,0 +1,4 @@
+return {
+
+  { "https://github.com/typicode/bg.nvim" },
+}
---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  -- -- Add Kanagawa
+  -- {
+  --   "rebelot/kanagawa.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  -- },
+  --
+  -- -- Configure LazyVim to load kanagawa-dragon
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "kanagawa-wave", -- Fixed typo here
+  --   },
+  -- },
+}
